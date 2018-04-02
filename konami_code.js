39ae36ec917c0de4f2451dbf551bf3707668af9f
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-04-02 15:58:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,9 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const input = document.querySelector('input')
+  const input = document.body
  
-input.addEventListener('keydown', function(e) {
+  input.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
 }
